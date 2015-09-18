7a6b80a9cb13c31cbf3c7c9005311611234cb7c1
David Hayes <drhayes@gmail.com>
2015-09-18 16:11:48 -0500

    The very beginnings of a game instance and some states.

---
+++ src/game.js
@@ -0,0 +1,30 @@
+'use strict';
+
+if (FULL) {
+  require('../vendor/phaser.min.js');
+} else {
+  require('../vendor/phaser.js');
+}
+require('../index.html');
+
+var Boot = require('./states/boot');
+var Preload = require('./states/preload');
+var MainMenu = require('./states/mainMenu');
+var Main = require('./states/main');
+
+var game = window.game = new Phaser.Game({
+  width: 1280,
+  height: 960,
+  renderer: Phaser.AUTO,
+  state: new Boot(),
+  transparent: false,
+  antialias: false,
+  canvasStyle: ''
+});
+game.state.add('preload', new Preload());
+game.state.add('mainMenu', new MainMenu());
+game.state.add('main', new Main());
+
+// Ugh.
+game.onTake = new Phaser.Signal();
+game.onRestore = new Phaser.Signal();
