8466d1147250ac5afd93c33c1e70bac62e11f169
David Hayes <drhayes@gmail.com>
2015-09-20 22:10:47 -0500

    A guard.

---
+++ src/sprites/guard.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var fs = require('fs');
+import enableBehaviors from '../behaviors/enableBehaviors';
+
+export default class Guard extends Phaser.Sprite {
+  constructor(game, x, y) {
+    super(game, x, y, 'player', 0);
+    game.physics.arcade.enable(this);
+    enableBehaviors(this);
+
+    this.anchor.setTo(0.5);
+    this.body.width = 30;
+    this.body.height = 30;
+    this.body.collideWorldBounds = true;
+    this.tint = 0xcb0404;
+
+    let glow = new Phaser.Filter(game, null, fs.readFileSync(__dirname + '/../shaders/glow.frag', 'utf8'));
+    this.filters = [glow];
+  }
+
+  update() {
+    this.behave('update');
+  }
+};
--- src/states/main.js
+++ src/states/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 import Player from '../sprites/player';
+import Guard from '../sprites/guard';
 
 export default class Main extends Phaser.State {
   create() {
     this.player = new Player(this.game, 200, 200);
     this.game.add.existing(this.player);
+
+    let guard = new Guard(this.game, 800, 500);
+    this.game.add.existing(guard);
   }
 };
