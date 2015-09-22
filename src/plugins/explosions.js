cd81d97dc42ccb94542677065ef96578c5d21836
David Hayes <drhayes@gmail.com>
2015-09-21 21:07:15 -0500

    Gimme the bits!

--- src/plugins/explosions.js
+++ src/plugins/explosions.js
@@ -33,10 +33,28 @@ export default class Explosions extends Phaser.Plugin {
     mediumExplosions.height = 1;
     mediumExplosions.setAll('tint', 0xffff00);
     this.mediumExplosions = mediumExplosions;
+
+    let bitsExplosions = this.game.add.emitter(0, 0, NUM_EXPLOSIONS);
+    bitsExplosions.makeParticles('player', 1);
+    bitsExplosions.setAlpha(1, 0, EXPLOSION_LIFETIME_MS * 3, Phaser.Easing.Quadratic.Out);
+    bitsExplosions.setScale(2, 1, 2, 1, EXPLOSION_LIFETIME_MS);
+    bitsExplosions.setRotation(0, 1);
+    bitsExplosions.width = 1;
+    bitsExplosions.height = 1;
+    bitsExplosions.setAll('tint', 0xffff00);
+    bitsExplosions.setAll('body.allowGravity', false);
+    this.bitsExplosions = bitsExplosions;
   }
 
   update() {
     this.smallBoomDelay -= this.game.time.physicsElapsedMS;
+  }
+
+  bits(x, y, num) {
+    num = num || 20;
+    this.bitsExplosions.x = x;
+    this.bitsExplosions.y = y;
+    this.bitsExplosions.start(true, EXPLOSION_LIFETIME_MS, null, num);
   }
 
   small(x, y, num) {
@@ -54,6 +72,7 @@ export default class Explosions extends Phaser.Plugin {
     this.mediumExplosions.x = x;
     this.mediumExplosions.y = y;
     this.mediumExplosions.start(true, EXPLOSION_LIFETIME_MS, null, 5);
+    this.bits(x, y);
     this.mediumBoom.play();
   }
 };
