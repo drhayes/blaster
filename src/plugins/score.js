e2930183838589d8a4dbf9c28976440245e731cb
David Hayes <drhayes@gmail.com>
2015-10-02 15:47:16 -0500

    Show a little score number for every enemy killed.

--- src/plugins/score.js
+++ src/plugins/score.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const EXTRA_LIFE = 7500;
+const NUM_SCORES = 10;
 
 export default class Score {
   constructor(game) {
@@ -13,6 +14,14 @@ export default class Score {
     this.game.world.bringToTop(this.scoreText);
     this.updateScore();
     this.extraLifeSound = this.game.add.audio('extraLife');
+
+    this.scorePool = this.game.add.group();
+    for (let i = 0; i < NUM_SCORES; i++) {
+      let scoreText = this.game.add.bitmapText(0, 0, 'computerPixelFont', '0', 24);
+      scoreText.anchor.set(0.5);
+      scoreText.alive = scoreText.exists = scoreText.visible = false;
+      this.scorePool.addChild(scoreText);
+    }
   }
 
   updateScore() {
@@ -21,10 +30,22 @@ export default class Score {
   }
 
   killed(thing) {
+    console.log(thing.x, thing.y);
     if (this.current % EXTRA_LIFE > (this.current + thing.score) % EXTRA_LIFE) {
       this.game.lives.lives++;
       this.game.lives.updateLivesImage();
       this.extraLifeSound.play();
+    }
+    let score = this.scorePool.getFirstExists(false);
+    if (score) {
+      score.reset(thing.x, thing.y);
+      score.text = thing.score;
+      this.game.add.tween(score).to({
+        y: thing.y - 30
+      }, 1000, Phaser.Easing.Quadratic.Out, true)
+        .onComplete.add(() => {
+          score.kill();
+        });
     }
     this.current += thing.score;
     this.updateScore();
