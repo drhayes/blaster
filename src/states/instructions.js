75c35d23a3a4e5ee5db76165e9254442e0e6e50d
David Hayes <drhayes@gmail.com>
2015-10-19 22:06:43 -0500

    Instructions seem complete.

--- src/states/instructions.js
+++ src/states/instructions.js
@@ -7,6 +7,16 @@ import BlasterButton from '../sprites/blasterButton';
 export default class Instructions extends BaseIntro {
   create() {
     super.create();
+
+    // makeText(y, text, size) {
+    this.makeText(this.game.world.centerY * 0.5, 'Shoot everything. Know your enemy.');
+    this.makeText(this.game.world.centerY * 0.6, 'Extra life every 10,000.');
+
+    this.game.add.existing(new BlasterButton(this.game, this.game.world.centerX, this.game.world.centerY * 1.2, 'Main Menu', () => {
+      this.game.state.start('mainMenu');
+    }));
+
+
     tracking.instructions();
   }
 }
